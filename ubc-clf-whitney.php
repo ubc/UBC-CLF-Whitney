9730c1c33324900f100a85a67554d9ffbe557cf1
Flynn <fsquaredesign@gmail.com>
2024-04-19 15:10:25 -0700

    coding standard fixes

--- ubc-clf-whitney.php
+++ ubc-clf-whitney.php
@@ -8,11 +8,11 @@
  * Description:       Add CLF Whitney webfont CSS request. <strong>Note: Required registration</strong>. Please sign up on <a href="http://brand.ubc.ca/font-request-form/" target="_blank">UBC Brand</a> website.
  * Version:           1.1
  * Author:            Michael Kam & Flynn O'Connor
- * Author URI:        
+ * Author URI:
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
- * Text Domain:		  ubc-clf-whitney
-*/
+ * Text Domain:       ubc-clf-whitney
+ */
 
 class UBC_CLF_Whitney {
 
@@ -31,7 +31,8 @@ class UBC_CLF_Whitney {
 		// Add our actions/filters
 		$this->add_actions();
 
-	}/* init() */
+	}//end init()
+
 
 
 
@@ -50,9 +51,10 @@ class UBC_CLF_Whitney {
 		add_action( 'init', array( $this, 'init__add_clf_whitney' ) );
 
 		// Add new settings panels
-		add_action('admin_init', array($this, 'register_settings'));
+		add_action( 'admin_init', array( $this, 'register_settings' ) );
 
-	}/* add_actions() */
+	}//end add_actions()
+
 
 
 
@@ -67,57 +69,59 @@ class UBC_CLF_Whitney {
 
 	public function init__add_clf_whitney() {
 
-		$font_source = get_option('ubc_font_source', 'production'); // Default to 'production' if not set
+		$font_source = get_option( 'ubc_font_source', 'production' ); // Default to 'production' if not set
 
-        if ($font_source === 'development') {
+		if ( $font_source === 'development' ) {
 			wp_enqueue_style( 'ubc-clf-whitney', 'https://cloud.typography.com/6804272/697806/css/fonts.css' );
-        } else {
+		} else {
 			wp_enqueue_style( 'ubc-clf-whitney', 'https://cloud.typography.com/6804272/781004/css/fonts.css' );
-        }
+		}
 
-	}/* init__add_clf_whitney() */
+	}//end init__add_clf_whitney()
+
 
 	/**
 	 * Method for registering new settings panel;
 	 *
 	 * @since 1.0.1
-	 * 
+	 *
 	 * @return void
 	 */
-    public function register_settings() {
-        register_setting('reading', 'ubc_font_source');
+	public function register_settings() {
+		register_setting( 'reading', 'ubc_font_source' );
 
-        add_settings_section(
-            'ubf_settings_section',
-            'Font Source Environment',
-            array($this, 'settings_section_callback'),
-            'reading'
-        );
+		add_settings_section(
+			'ubf_settings_section',
+			'Font Source Environment',
+			array( $this, 'settings_section_callback' ),
+			'reading'
+		);
 
-        add_settings_field(
-            'ubf_font_source_field',
-            'Select Environment',
-            array($this, 'font_source_field_callback'),
-            'reading',
-            'ubf_settings_section'
-        );
-    }
+		add_settings_field(
+			'ubf_font_source_field',
+			'Select Environment',
+			array( $this, 'font_source_field_callback' ),
+			'reading',
+			'ubf_settings_section'
+		);
+	}
 
 	public function settings_section_callback() {
-        echo '<p>Select whether the website is for development/testing or production purposes.</p>';
-    }
+		echo '<p>Select whether the website is for development/testing or production purposes.</p>';
+	}
 
-    public function font_source_field_callback() {
-        $font_source = get_option('ubc_font_source');
-        ?>
-        <input type="radio" id="production" name="ubc_font_source" value="production" <?php checked($font_source, 'production'); ?>>
-        <label for="production">Production</label><br>
-        <input type="radio" id="development" name="ubc_font_source" value="development" <?php checked($font_source, 'development'); ?>>
-        <label for="development">Development</label>
-        <?php
-    }
+	public function font_source_field_callback() {
+		$font_source = get_option( 'ubc_font_source' );
+		?>
+		<input type="radio" id="production" name="ubc_font_source" value="production" <?php checked( $font_source, 'production' ); ?>>
+		<label for="production">Production</label><br>
+		<input type="radio" id="development" name="ubc_font_source" value="development" <?php checked( $font_source, 'development' ); ?>>
+		<label for="development">Development</label>
+		<?php
+	}
 
-}/* class UBC_CLF_Whitney */
+}//end class
+
 
 
 add_action( 'plugins_loaded', 'plugins_loaded__init_ubcclfwhitney' );
@@ -127,4 +131,5 @@ function plugins_loaded__init_ubcclfwhitney() {
 	$clf_whitney = new UBC_CLF_Whitney();
 	$clf_whitney->init();
 
-}/* plugins_loaded__init_ubcclfwhitney() */
+}//end plugins_loaded__init_ubcclfwhitney()
+
